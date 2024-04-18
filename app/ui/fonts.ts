b916fc21298cc8f64518e7a1436e77fe34f9843c
Daniel Grandoli <danig2994@gmail.com>
2024-04-18 12:46:55 -0300

    practica

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import {Inter} from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { Lora } from 'next/font/google';
+
+export const inter= Inter({subsets:['latin']});
+export const lora = Lora({subsets:['latin']});
+export const lusitana = Lusitana({weight:["400","700"],subsets:['latin']})
